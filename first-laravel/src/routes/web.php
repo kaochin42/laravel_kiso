45575cd3e605c688767e46c0b577ff0f8642f11d
kaochin <kzk4289@gmail.com>
2026-03-03 15:37:08 +0900

    ブラウザに画面表示をしよう:controller編

--- first-laravel/src/routes/web.php
+++ first-laravel/src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\HelloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,6 @@ use App\Http\Controllers\TestController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', [TestController::class, 'index']);
+
+Route::get('/test', [TestController::class, 'index']);
+Route::get('/hello', [HelloController::class, 'index']);
