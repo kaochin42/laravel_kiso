3d197be9fc187ce3416c911f25958505e5a1b9b7
kaochin <kzk4289@gmail.com>
2026-03-18 16:21:23 +0900

    ミドルウェアを使用してみよう

--- advance-laravel/src/app/Http/Controllers/BookController.php
+++ advance-laravel/src/app/Http/Controllers/BookController.php
@@ -8,7 +8,8 @@ use App\Models\Book;
 class BookController extends Controller
 {
     public function index(){
-        $items = Book::all();
+        // $items = Book::all();
+        $items = Book::with('author')->get();
         return view('book.index', ['items'=>$items]);
     }
     public function add(){
--- first-laravel/src/routes/web.php
+++ first-laravel/src/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\MiddlewareController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,7 @@ use App\Http\Controllers\HelloController;
 #Route::get('/test', [TestController::class, 'index']);
 Route::get('/hello', [HelloController::class, 'index']);
 Route::get('/', [TestController::class, 'index']);
+Route::post('/',[TestController::class, 'post']);
+Route::get('/middleware', [MiddlewareController::class, 'index']);
+// Route::post('/middleware', [MiddlewareController::class, 'post'])->middleware('first');
+Route::post('/middleware', [MiddlewareController::class, 'post']);
