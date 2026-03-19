962ca369172e29dd846d80505597c44f2e78b5ec
kaochin <kzk4289@gmail.com>
2026-03-20 08:54:36 +0900

    セッションについて学ぼう

--- advance-laravel/src/routes/web.php
+++ advance-laravel/src/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,3 +36,5 @@ Route::prefix('book')->group(function () {
     Route::post('/add', [BookController::class, 'create']);
 });
 Route::get('/relation', [AuthorController::class, 'relate']);
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
